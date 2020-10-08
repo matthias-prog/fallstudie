c1cb63dbcb552b25654da13d48db114799c5316d
julianhermle <60342199+julianhermle@users.noreply.github.com>
2020-10-08 16:30:54 +0200

    Update MainAdmin.java

--- src/frontend/MainAdmin.java
+++ src/frontend/MainAdmin.java
@@ -31,14 +31,12 @@ public class MainAdmin extends JFrame {
 	private JFrame mainAdminFrame;
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField txtSuchfeld;
 	private JTable tableCITypen;
 	private JButton btnAbmelden;
-	private JButton btnSuche;
 	private JScrollPane scrollPane;
-	private JButton btnCiTypVerwaltung;
-	private JButton btnZurückMain;
-	private JButton btnAenderePasswort;
+	private JButton btnCiTypHinzufügen;
+	private JButton btnCiTypAendern;
+	private JButton btnCiTypLoeschen;
 
 	/**
 	 * Launch the application.
@@ -75,34 +73,17 @@ public class MainAdmin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		//Image img = new ImageIcon(this.getClass().getResource("/Favicon.png")).getImage();
-		//mainFrame.setIconImage(img);
-
-		
-		txtSuchfeld = new JTextField();
-		txtSuchfeld.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtSuchfeld.setBorder(new LineBorder(new Color(171, 173, 179)));
-		txtSuchfeld.setBounds(17, 60, 600, 50);
-		contentPane.add(txtSuchfeld);
-		txtSuchfeld.setColumns(10);
 		
 		btnAbmelden = new JButton("Abmelden");
 		btnAbmelden.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnAbmelden.setBorder(new LineBorder(new Color(0, 0, 0)));
 		btnAbmelden.setBackground(Color.WHITE);
-		btnAbmelden.setBounds(870, 60, 175, 50);
+		btnAbmelden.setBounds(873, 623, 175, 50);
 		contentPane.add(btnAbmelden);
-		
-		btnSuche = new JButton("\uD83D\uDD0D");
-		btnSuche.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 20));
-		btnSuche.setBorder(new LineBorder(new Color(0, 0, 0)));
-		btnSuche.setBackground(Color.WHITE);
-		btnSuche.setBounds(622, 60, 50, 50);
-		contentPane.add(btnSuche);
 		
 		scrollPane = new JScrollPane();
 		scrollPane.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		scrollPane.setBounds(17, 160, 840, 513);
+		scrollPane.setBounds(17, 60, 840, 613);
 		contentPane.add(scrollPane);
 		
 		tableCITypen = new JTable();
@@ -146,63 +127,37 @@ public class MainAdmin extends JFrame {
 		btnBenutzerverwaltung.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnBenutzerverwaltung.setBorder(new LineBorder(new Color(0, 0, 0)));
 		btnBenutzerverwaltung.setBackground(Color.WHITE);
-		btnBenutzerverwaltung.setBounds(873, 160, 175, 50);
+		btnBenutzerverwaltung.setBounds(873, 563, 175, 50);
 		contentPane.add(btnBenutzerverwaltung);
 		
-		//Button "CI-Typ Verwaltung"
-		btnCiTypVerwaltung = new JButton("CI-Typ Verwaltung");
-		btnCiTypVerwaltung.addActionListener(new ActionListener() {
+		//Button "CI-Typ Hinzufügen"
+		btnCiTypHinzufügen = new JButton("CI-Typ Hinzufügen");
+		btnCiTypHinzufügen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//TODO: ActionEvent: Funktionalität hinzufügen
 			}
 		});
-		btnCiTypVerwaltung.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnCiTypVerwaltung.setBorder(new LineBorder(new Color(0, 0, 0)));
-		btnCiTypVerwaltung.setBackground(Color.WHITE);
-		btnCiTypVerwaltung.setBounds(873, 240, 175, 50);
-		contentPane.add(btnCiTypVerwaltung);
+		btnCiTypHinzufügen.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnCiTypHinzufügen.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnCiTypHinzufügen.setBackground(Color.WHITE);
+		btnCiTypHinzufügen.setBounds(873, 60, 175, 50);
+		contentPane.add(btnCiTypHinzufügen);
 		
-		//Button "Zurück zur Startseite"
-		btnZurückMain = new JButton("Zurück zur Startseite");
-		btnZurückMain.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//TODO: ActionEvent: Funktionalität hinzufügen
-				mainAdminFrame.dispose();
-				Main startseite = new Main();
-				startseite.setVisible(true);
-			}
-		});
-		btnZurückMain.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnZurückMain.setBorder(new LineBorder(new Color(0, 0, 0)));
-		btnZurückMain.setBackground(Color.WHITE);
-		btnZurückMain.setBounds(873, 320, 175, 50);
-		contentPane.add(btnZurückMain);
+		//Button "CI-Typ Ändern"
+		btnCiTypAendern = new JButton("CI-Typ Bearbeiten");
+		btnCiTypAendern.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnCiTypAendern.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnCiTypAendern.setBackground(Color.WHITE);
+		btnCiTypAendern.setBounds(873, 120, 175, 50);
+		contentPane.add(btnCiTypAendern);
 		
-		
-		//Button "Passwort Ändern"
-		btnAenderePasswort = new JButton("Passwort ändern");
-		btnAenderePasswort.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {				
-				//TODO: ActionEvent: Funktionalität hinzufügen
-			}
-		});
-		btnAenderePasswort.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnAenderePasswort.setBorder(new LineBorder(new Color(0, 0, 0)));
-		btnAenderePasswort.setBackground(Color.WHITE);
-		btnAenderePasswort.setBounds(873, 424, 175, 50);
-		contentPane.add(btnAenderePasswort);
-		
-		JButton btnNeuerRecord = new JButton("Neuer CI-Record");
-		btnNeuerRecord.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//TODO: ActionEvent: Funktionalität hinzufügen
-			}
-		});
-		btnNeuerRecord.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNeuerRecord.setBorder(new LineBorder(new Color(0, 0, 0)));
-		btnNeuerRecord.setBackground(Color.WHITE);
-		btnNeuerRecord.setBounds(682, 60, 175, 50);
-		contentPane.add(btnNeuerRecord);
+		//Button "CI-Typ Löschen"
+		btnCiTypLoeschen = new JButton("CI-Typ Löschen");
+		btnCiTypLoeschen.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnCiTypLoeschen.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnCiTypLoeschen.setBackground(Color.WHITE);
+		btnCiTypLoeschen.setBounds(873, 180, 175, 50);
+		contentPane.add(btnCiTypLoeschen);
 		
 		
 	}
